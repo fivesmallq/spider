52f82b1192decfd32bf875d374e4846dfb998b54
fivesmallq <fivesmallq@gmail.com>
2016-02-08 18:03:29 +0800

    add actor

---
+++ src/main/java/im/nll/data/spider/actor/SchedulingActor.java
@@ -0,0 +1,9 @@
+package im.nll.data.spider.actor;
+
+/**
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/2/8 下午6:01
+ */
+public class SchedulingActor {
+}
