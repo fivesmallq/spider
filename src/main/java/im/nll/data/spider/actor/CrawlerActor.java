e3b9e26c43e8331ee3aa618db7c5faad9e1ef6a5
fivesmallq <fivesmallq@gmail.com>
2016-02-13 17:23:07 +0800

    add akka actor deps

--- src/main/java/im/nll/data/spider/actor/CrawlerActor.java
+++ src/main/java/im/nll/data/spider/actor/CrawlerActor.java
@@ -1,9 +1,15 @@
 package im.nll.data.spider.actor;
 
+import akka.actor.UntypedActor;
+
 /**
  * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
  * @version Revision: 1.0
  * @date 16/2/8 下午6:01
  */
-public class CrawlerActor {
+public class CrawlerActor extends UntypedActor {
+    @Override
+    public void onReceive(Object o) throws Exception {
+
+    }
 }
